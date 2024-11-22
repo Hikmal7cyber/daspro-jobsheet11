ee3fb8d76dac713ebe2f2b8229b108ea386d20b8
Hikmal7cyber <mhikmal.ag@gmail.com>
2024-11-22 15:55:19 +0700

    Pengunjung Cafe 15 kode foreach loop

--- PengunjungCafe15.java
+++ PengunjungCafe15.java
@@ -1,8 +1,8 @@
 public class PengunjungCafe15 {
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String name : namaPengunjung) {
+            System.out.println("- " + name);
         }
     }
 
